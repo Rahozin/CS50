88ba45c2f3ea1fc3f886be001b37b9ee251eb375
Dmytro <RagozinDY@gmail.com>
2022-10-21 14:03:06 +0300

    clear_db

--- pset8/finance/-- SQLite2.sql
+++ pset8/finance/-- SQLite2.sql
@@ -1,18 +1,11 @@
 -- SQLite
-CREATE TABLE shares2 (
-    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-    name VARCHAR(255) NOT NULL,
-    symbol CHAR(8) NOT NULL UNIQUE
-);
-
 INSERT INTO shares2 SELECT * FROM shares;
 
-DROP TABLE shares;
 
 ALTER TABLE shares2 RENAME TO shares;
 
---
+
 DELETE FROM orders WHERE id > 0;
 
---
+
 SELECT id FROM shares WHERE symbol = 'IBM';
